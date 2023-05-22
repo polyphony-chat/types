6ee21ef2279a2ab7303b2723b165ebf1bb6b75aa
Zertex <me@zertex.dev>
2023-05-22 15:54:19 -0400

    add config types

---
+++ src/config/types/kafka_configuration.rs
@@ -0,0 +1,9 @@
+use serde::{Deserialize, Serialize};
+
+use crate::config::types::subconfigs::kafka::KafkaBroker;
+
+#[derive(Debug, Default, PartialEq, Eq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct KafkaConfiguration {
+    pub brokers: Vec<KafkaBroker>,
+}
