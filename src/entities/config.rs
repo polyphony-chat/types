386099e7081ca05fd2720049b1e79c81e155259f
Zertex <me@zertex.dev>
2023-05-23 11:32:05 -0400

    make the Config::value optional to support NULL values in database

--- src/entities/config.rs
+++ src/entities/config.rs
@@ -9,23 +9,29 @@ use crate::errors::Error;
 #[cfg_attr(feature = "sqlx", derive(FromRow))]
 pub struct ConfigEntity {
     pub key: String,
-    pub value: Value,
+    pub value: Option<Value>,
 }
 
 impl ConfigEntity {
-    pub fn as_string(&self) -> String {
-        self.value
-            .as_str()
-            .expect("value is not a string")
-            .to_string()
+    pub fn as_string(&self) -> Option<String> {
+        let Some(v) = self.value.as_ref() else {
+            return None;
+        };
+        Some(v.as_str().expect("value is not a string").to_string())
     }
 
-    pub fn as_bool(&self) -> bool {
-        self.value.as_bool().expect("value is not a boolean")
+    pub fn as_bool(&self) -> Option<bool> {
+        let Some(v) = self.value.as_ref() else {
+            return None;
+        };
+        Some(v.as_bool().expect("value is not a boolean"))
     }
 
-    pub fn as_int(&self) -> i64 {
-        self.value.as_i64().expect("value is not a number")
+    pub fn as_int(&self) -> Option<i64> {
+        let Some(v) = self.value.as_ref() else {
+            return None;
+        };
+        Some(v.as_i64().expect("value is not a number"))
     }
 }
 
