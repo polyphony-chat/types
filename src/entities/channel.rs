c98562a17b98a583f3df108c34d0472a1616d9ae
Zertex <me@zertex.dev>
2023-05-24 05:00:18 -0400

    add ChannelType enum

--- src/entities/channel.rs
+++ src/entities/channel.rs
@@ -9,7 +9,7 @@ use crate::{
 pub struct Channel {
     pub id: Snowflake,
     #[serde(rename = "type")]
-    pub channel_type: i32,
+    pub channel_type: ChannelType,
     pub guild_id: Option<String>,
     pub position: Option<i32>,
     pub permission_overwrites: Option<Vec<PermissionOverwrite>>,
@@ -85,4 +85,31 @@ pub struct ThreadMember {
 pub struct DefaultReaction {
     pub emoji_id: Option<String>,
     pub emoji_name: Option<String>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "SCREAMING_SNAKE_CASE")]
+#[repr(i32)]
+pub enum ChannelType {
+    GuildText = 0,
+    Dm = 1,
+    GuildVoice = 2,
+    GroupDm = 3,
+    GuildCategory = 4,
+    GuildNews = 5,
+    GuildStore = 6,
+    Encrypted = 7,
+    EncryptedThreads = 8,
+    Transactional = 9,
+    GuildNewsThread = 10,
+    GuildPublicThread = 11,
+    GuildPrivateThread = 12,
+    GuildStageVoice = 13,
+    Directory = 14,
+    GuildForum = 15,
+    TicketTracker = 33,
+    Kanban = 34,
+    VoicelessWhiteboard = 35,
+    CustomStart = 64,
+    Unhandled = 255,
 }
